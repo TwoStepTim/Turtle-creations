12b788ec6afdb37787db45607ae92193a9cc7032
TwoStepTim <tjmims33@gmail.com>
2024-03-05 16:12:59 -0500

    Shopping Page

--- script.js
+++ script.js
@@ -20,11 +20,21 @@ hamburger.addEventListener('click', () => {
         hamburger.style.color = 'rgb(0, 0, 0)';
     }
 }
-
-
-
-
-
-
-
 );
+
+function filterProducts(category) {
+    const products = document.querySelectorAll('.product');
+
+    products.forEach(product => {
+        if (category === 'all') {
+            product.style.display = 'block';
+        } else {
+            if (product.classList.contains(category)) {
+                product.style.display = 'block';
+            } else {
+                product.style.display = 'none';
+            }
+        }
+    });
+}
+
